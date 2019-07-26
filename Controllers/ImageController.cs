2f3f43cfbb3ef8b8a0f664ac93525c3fa4acceef
vvnha <vvnha.17it3@sict.udn.vn>
2019-07-26 16:46:30 +0700

    first commit

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace UpServer.Controllers
+{
+    [RoutePrefix("api/image")]
+    public class ImageController : ApiController
+    {
+        SqlCommand cmd;
+        SqlConnection con;
+        [Route("upimg")]
+        [HttpPost]
+        public HttpResponseMessage Upload()
+        {
+            try
+            {
+                var request = HttpContext.Current.Request;
+                var description = request.Form["description"];
+                var photo = request.Files["photo"];
+                var imgPath = HttpContext.Current.Server.MapPath("~/Content/Uploads/" + photo.FileName);
+                AddImage(photo.FileName);
+                photo.SaveAs(imgPath);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            catch
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpGet]
+        public List<upimage> GetFoodLists()
+        {
+            DBImageDataContext db = new DBImageDataContext();
+            return db.upimages.ToList();
+        }
+        [HttpGet]
+        public upimage GetFood(int id)
+        {
+            DBImageDataContext db = new DBImageDataContext();
+            return db.upimages.FirstOrDefault(x => x.imgID == id);
+        }
+        public void AddImage(String name)
+        {
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=desktop-8n0o4eo\mssqlserver1;Database=ImageUpload;User ID=sa;Password=nhavovvv;";
+            //SqlCommand sqlCmd = new SqlCommand("INSERT INTO tblEmployee (EmployeeId,Name,ManagerId) Values (@EmployeeId,@Name,@ManagerId)", myConnection);  
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "INSERT INTO upimage Values (@Name)";
+            sqlCmd.Connection = myConnection;
+
+            sqlCmd.Parameters.AddWithValue("@Name", name);
+
+            myConnection.Open();
+            int rowInserted = sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UpServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UpServer")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1b76fab9-2da8-4432-9ae1-dc1bac730aa1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
